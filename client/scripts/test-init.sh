0edc92962b748506ac0820ea19a32135d11c157c
iofirag <iofirag@gmail.com>
2026-05-02 21:45:49 +0300

    feat: add immichframe service to docker-compose and update test-init script

--- client/scripts/test-init.sh
+++ client/scripts/test-init.sh
@@ -1,3 +1,3 @@
 # $ DATA_DIR=./client-data ./scripts/init-client.sh
-# docker compose up -d
-# $ DATA_DIR=./client-data IMMICH_API_KEY="351UB2wZY5cMxAoBeWOJZrLjo8D2NvviGui6lDpKlLc" ALBUMS="3ad091b7-8734-4d67-a8ef-df0efe6f13e7,0134c00c-1868-4ef9-9c93-0ee5120fe768" ./scripts/gen-immichframe-config.sh
+# $ docker compose up -d
+# $ DATA_DIR=./client-data IMMICH_API_KEY="AtKROwCZsyr0q23lnR6Y7KBZ7bpjnNm3RvquSUbrY" ALBUMS="465e0a9e-6648-4f3f-9e23-423e631a3603,465e0a9e-6648-4f3f-9e23-423e631a3603" ./scripts/gen-immichframe-config.sh
